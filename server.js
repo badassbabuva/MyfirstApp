90a540a6bab72bff088f018a1f71cc381c6c5f5f
Priyansh Parmar <91priyansh@gmail.com>
2018-05-28 11:13:29 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');//handlebars to handle template 
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');//to inject partials
+
+hbs.registerHelper('screamIt',(text) => {
+
+	return text.toUpperCase();
+});
+
+app.set('view engine','hbs');
+
+app.get('/',(req,res)=>{
+
+	res.render('index.hbs',{
+
+		welcomeMessage: 'welcome to my first express project',
+		currentYear: new Date().getFullYear()
+	});
+
+});
+
+app.get('/about',(req,res)=>{
+
+	res.render('about.hbs',{
+
+		pageTitle:'About Page',
+		currentYear: new Date().getFullYear()
+
+	});
+
+});
+
+
+
+app.listen(port);
